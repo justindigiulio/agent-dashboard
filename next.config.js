79c04e4dd8562b0a737802d8309d02ffd27f81b9
justindigiulio <justindigiulio@gmail.com>
2025-08-28 14:31:50 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // no headers for now—keep it minimal to confirm deploy
+};
+
+module.exports = nextConfig;
