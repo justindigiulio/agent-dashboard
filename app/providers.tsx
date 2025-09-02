52e5f0bb0c1e8a8e9813fe62228585ee718db771
justindigiulio <justindigiulio@gmail.com>
2025-09-02 14:00:00 -0400

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,5 @@ import { SessionProvider } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
 
 export default function Providers({ children }: PropsWithChildren) {
-  // SessionProvider enables useSession() inside client components like ChatWidget
   return <SessionProvider>{children}</SessionProvider>;
 }
