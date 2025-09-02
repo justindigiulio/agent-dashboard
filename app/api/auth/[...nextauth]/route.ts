cbeb5c7fc5a2116294dca3851525e8ca5de2c851
justindigiulio <justindigiulio@gmail.com>
2025-09-02 12:55:14 -0400

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,7 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-// use a relative path to avoid alias issues
 import { authOptions } from "../../../../lib/auth";
 
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
