e0c75723653fdd43d82c270df1e869d79abb5ca3
justindigiulio <justindigiulio@gmail.com>
2025-09-15 18:43:32 -0400

    Create route.ts

---
+++ app/api/leads/unclaimed/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from 'next/server';
+import { getUnclaimed } from '@/lib/mockLeads';
+
+export async function GET() {
+  return NextResponse.json({ leads: getUnclaimed() });
+}
