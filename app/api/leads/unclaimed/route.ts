7ac88bb06929fdc9b1658520000c4669f5002936
justindigiulio <justindigiulio@gmail.com>
2025-09-26 13:07:33 -0400

    Update route.ts

--- app/api/leads/unclaimed/route.ts
+++ app/api/leads/unclaimed/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getUnclaimed } from '@/lib/mockLeads';
+import { getUnclaimed } from '../../../../lib/mockLeads';
 
 
 export async function GET(){
